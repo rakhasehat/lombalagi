95a244c9addfbeaff627b8787542270d56db1b97
M. Rakha Albaihaqi Nasrudin <m_rakha_26rpl@student.smktelkom-mlg.sch.id>
2018-11-15 13:13:38 +0700

    Signed-off-by: M. Rakha Albaihaqi Nasrudin <m_rakha_26rpl@student.smktelkom-mlg.sch.id>

---
+++ prosesdaftar.php
@@ -0,0 +1,25 @@
+<?php
+   require_once("koneksi.php");
+   $username = $_POST['username'];
+   $pass = $_POST['password'];
+   $nama = $_POST['nama'];
+   $kelas = $_POST['kelas'];
+   
+   $sql = "SELECT * FROM user WHERE username = '$username'";
+   $query = $db->query($sql);
+   if($query->num_rows != 0) {
+     echo "<div align='center'>Username Sudah Terdaftar! <a href='daftar.php'>Back</a></div>";
+   } else {
+     if(!$username || !$pass) {
+       echo "<div align='center'>Masih ada data yang kosong! <a href='daftar.php'>Back</a>";
+     } else {
+       $data = "INSERT INTO user(id, username, password, nama, kelas) VALUES (NULL, '$username', '$pass', '$nama', '$kelas')";
+       $simpan = $db->query($data);
+       if($simpan) {
+         echo "<div align='center'>Pendaftaran Sukses, Silahkan <a href='login.php'>Login</a></div>";
+       } else {
+         echo "<div align='center'>Proses Gagal!</div>";
+       }
+     }
+   }
+?>
